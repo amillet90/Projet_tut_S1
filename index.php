b66e2ae1c6f22713e760b33bdcaa6ad45a893c16
amillet.iut90 <amillet.iut90@gmail.com>
2020-06-26 17:32:14 +0200

    make it better

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>permier test</h1>
 <?php
 
-echo 'bonjour';
-
+for($i;$i<15;$i++)
+{
+    for($j;$j<$i;$j++)
+        echo '+';
+    echo "<br>";
+}
 ?>
+</body>
+</html>
+
+
+
+
