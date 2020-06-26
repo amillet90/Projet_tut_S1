3012111bf78c3f1820a58b1f138241bee7a0b11c
amillet.iut90 <amillet.iut90@gmail.com>
2020-06-26 17:45:35 +0200

    make it better

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/mesStyles.css">
     <title>Document</title>
 </head>
 <body>
 <h1>permier test</h1>
 <?php
-
-for($i;$i<15;$i++)
+for($i=0;$i<15;$i++)
 {
     for($j=0;$j<$i;$j++)
         echo '😃';
